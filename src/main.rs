74c49591b373a355415824be7e751866ee8a6251
Frank Taylor <fst@cadence.local>
2023-11-07 22:02:32 +0000

    Support window resize

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,53 @@ use log::*;
 mod grid;
 use grid::{GridCoord, SparseGrid};
 
-const CELL_SIZE: (f32, f32) = (10.0, 10.0);
-const GRID_SIZE: (usize, usize) = (100, 100);
-const VIEW_SIZE: (usize, usize) = (200, 150);
-const WINDOW_SIZE: (f32, f32) = (
-    CELL_SIZE.0 * VIEW_SIZE.0 as f32,
-    CELL_SIZE.1 * VIEW_SIZE.1 as f32,
-);
+const DEFAULT_WINDOW_SIZE: (f32, f32) = (2000.0, 1500.0);
+
+#[derive(Debug)]
+struct ViewParams {
+    window_size: (f32, f32),
+    cell_size: (f32, f32),
+    view_size: (usize, usize),
+    grid_size: (usize, usize),
+    line_width: f32,
+}
+
+impl ViewParams {
+    fn resize(&mut self, x: f32, y: f32) {
+        let vsx = x / self.cell_size.0;
+        let vsy = y / self.cell_size.1;
+        self.view_size = (vsx as usize, vsy as usize);
+        self.window_size = (x, y);
+    }
+}
+
+impl Default for ViewParams {
+    fn default() -> Self {
+        let mut vp = ViewParams {
+            window_size: DEFAULT_WINDOW_SIZE,
+            cell_size: (10.0, 10.0),
+            view_size: (0, 0),
+            grid_size: (100, 100),
+            line_width: 1.0,
+        };
+
+        vp.resize(vp.window_size.0, vp.window_size.1);
+
+        vp
+    }
+}
+
+// const CELL_SIZE: (f32, f32) = (10.0, 10.0);
+// const GRID_SIZE: (usize, usize) = (100, 100);
+// const VIEW_SIZE: (usize, usize) = (200, 150);
+// const WINDOW_SIZE: (f32, f32) = (
+//     CELL_SIZE.0 * VIEW_SIZE.0 as f32,
+//     CELL_SIZE.1 * VIEW_SIZE.1 as f32,
+// );
+// const LINE_WIDTH: f32 = 1.0;
+
 const BG_COLOR: Color = Color::WHITE;
 const CELL_COLOR: Color = Color::BLACK;
-const LINE_WIDTH: f32 = 1.0;
 const LINE_COLOR: Color = Color {
     r: 0.5,
     g: 0.5,
@@ -32,6 +69,7 @@ const LINE_COLOR: Color = Color {
 const TEXT_COLOR: Color = Color::BLACK;
 
 struct State {
+    view_params: ViewParams,
     grid: SparseGrid,
     show_grid: bool,
     fps: u32,
@@ -49,6 +87,7 @@ impl State {
     #[allow(unused)]
     pub fn new(ctx: &mut Context) -> Self {
         State {
+            view_params: ViewParams::default(),
             grid: SparseGrid::new(),
             show_grid: true,
             fps: 1,
@@ -66,10 +105,12 @@ impl State {
     #[allow(unused)]
     pub fn rand(ctx: &mut Context) -> Self {
         let mut s = State::new(ctx);
-        let off_x = (VIEW_SIZE.0 - GRID_SIZE.0) / 2;
-        let off_y = (VIEW_SIZE.1 - GRID_SIZE.1) / 2;
-        for x in off_x..(GRID_SIZE.0 + off_x) {
-            for y in off_y..(GRID_SIZE.1 + off_y) {
+        let view_size = s.view_params.view_size;
+        let grid_size = s.view_params.grid_size;
+        let off_x = (view_size.0 - grid_size.0) / 2;
+        let off_y = (view_size.1 - grid_size.1) / 2;
+        for x in off_x..(grid_size.0 + off_x) {
+            for y in off_y..(grid_size.1 + off_y) {
                 if rand::random() {
                     s.grid.set(GridCoord::Valid(x as i64, y as i64), 1);
                 }
@@ -82,7 +123,7 @@ impl State {
 
 impl EventHandler<GameError> for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        debug!("Update called");
+        //debug!("Update called");
         while ctx.time.check_update_time(self.fps) && self.running {
             debug!("Update accepted...");
 
@@ -109,50 +150,52 @@ impl EventHandler<GameError> for State {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        debug!("Draw called");
+        //debug!("Draw called");
         if !self.dirty {
             return Ok(());
         }
 
-        debug!("Dirty state to draw...");
+        //debug!("Dirty state to draw...");
         self.dirty = false;
 
         let mut canvas = graphics::Canvas::from_frame(ctx, BG_COLOR);
 
+        let view_params = &self.view_params;
+
         if self.show_grid {
-            for i in 0..VIEW_SIZE.0 as usize {
+            for i in 0..view_params.view_size.0 as usize {
                 let line = Mesh::new_line(
                     ctx,
                     &vec![
                         Point2 {
-                            x: i as f32 * CELL_SIZE.0,
+                            x: i as f32 * view_params.cell_size.0,
                             y: 0.0,
                         },
                         Point2 {
-                            x: i as f32 * CELL_SIZE.0,
-                            y: WINDOW_SIZE.1,
+                            x: i as f32 * view_params.cell_size.0,
+                            y: view_params.window_size.1,
                         },
                     ],
-                    LINE_WIDTH,
+                    view_params.line_width,
                     LINE_COLOR,
                 )?;
                 canvas.draw(&line, graphics::DrawParam::from(Point2 { x: 0.0, y: 0.0 }));
             }
 
-            for j in 0..VIEW_SIZE.1 as usize {
+            for j in 0..view_params.view_size.1 as usize {
                 let line = Mesh::new_line(
                     ctx,
                     &vec![
                         Point2 {
                             x: 0.0,
-                            y: j as f32 * CELL_SIZE.1,
+                            y: j as f32 * view_params.cell_size.1,
                         },
                         Point2 {
-                            x: WINDOW_SIZE.0,
-                            y: j as f32 * CELL_SIZE.1,
+                            x: view_params.window_size.0,
+                            y: j as f32 * view_params.cell_size.1,
                         },
                     ],
-                    LINE_WIDTH,
+                    view_params.line_width,
                     LINE_COLOR,
                 )?;
                 canvas.draw(&line, graphics::DrawParam::from(Point2 { x: 0.0, y: 0.0 }));
@@ -163,15 +206,19 @@ impl EventHandler<GameError> for State {
             if let GridCoord::Valid(x, y) = gc {
                 let x = x + self.xt;
                 let y = y + self.yt;
-                if x >= 0 && x < VIEW_SIZE.0 as i64 && y >= 0 && y < VIEW_SIZE.1 as i64 {
+                if x >= 0
+                    && x < view_params.view_size.0 as i64
+                    && y >= 0
+                    && y < view_params.view_size.1 as i64
+                {
                     let rect = Mesh::new_rectangle(
                         ctx,
                         DrawMode::fill(),
                         Rect::new(
-                            x as f32 * CELL_SIZE.0,
-                            y as f32 * CELL_SIZE.1,
-                            CELL_SIZE.0,
-                            CELL_SIZE.1,
+                            x as f32 * view_params.cell_size.0,
+                            y as f32 * view_params.cell_size.1,
+                            view_params.cell_size.0,
+                            view_params.cell_size.1,
                         ),
                         CELL_COLOR,
                     )?;
@@ -242,7 +289,7 @@ impl EventHandler<GameError> for State {
                 self.xt = 0;
                 self.yt = 0;
             }
-            if keycode == KeyCode::Delete {
+            if keycode == KeyCode::Delete || keycode == KeyCode::Back {
                 self.grid = SparseGrid::new();
                 self.generation = 0;
             }
@@ -256,6 +303,18 @@ impl EventHandler<GameError> for State {
 
         Ok(())
     }
+
+    fn resize_event(
+        &mut self,
+        _ctx: &mut Context,
+        width: f32,
+        height: f32,
+    ) -> Result<(), GameError> {
+        info!("Resize: {}, {}", width, height);
+
+        self.view_params.resize(width, height);
+        Ok(())
+    }
 }
 
 // Note: .env -> RUST_LOG=rusty_life=debug
@@ -264,7 +323,11 @@ fn main() -> GameResult {
     env_logger::init_from_env(env_logger::Env::default());
 
     let (mut ctx, event_loop) = ContextBuilder::new("Rusty Life", "Frank Taylor")
-        .window_mode(WindowMode::default().dimensions(WINDOW_SIZE.0, WINDOW_SIZE.1))
+        .window_mode(
+            WindowMode::default()
+                .dimensions(DEFAULT_WINDOW_SIZE.0, DEFAULT_WINDOW_SIZE.1)
+                .resizable(true),
+        )
         .build()?;
 
     let mut state = State::rand(&mut ctx);
