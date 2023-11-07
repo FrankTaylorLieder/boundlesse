542def029e92cb4cee419ae66e2a5533e4992845
Frank Taylor <fst@cadence.local>
2023-11-07 23:37:20 +0000

    Added zooming (A/S)

--- src/main.rs
+++ src/main.rs
@@ -18,45 +18,63 @@ const DEFAULT_WINDOW_SIZE: (f32, f32) = (2000.0, 1500.0);
 #[derive(Debug)]
 struct ViewParams {
     window_size: (f32, f32),
-    cell_size: (f32, f32),
-    view_size: (usize, usize),
-    grid_size: (usize, usize),
+    cell_size: f32,
+    view_size: (i64, i64),
+    grid_size: (i64, i64),
     line_width: f32,
+    xt: i64,
+    yt: i64,
 }
 
 impl ViewParams {
-    fn resize(&mut self, x: f32, y: f32) {
-        let vsx = x / self.cell_size.0;
-        let vsy = y / self.cell_size.1;
-        self.view_size = (vsx as usize, vsy as usize);
+    fn resize_aux(&mut self) {
+        let old_view_size = self.view_size;
+
+        let vsx = self.window_size.0 / self.cell_size + 1.0;
+        let vsy = self.window_size.1 / self.cell_size + 1.0;
+        self.view_size = (vsx as i64, vsy as i64);
+
+        let ((ovx, ovy), (nvx, nvy)) = (old_view_size, self.view_size);
+        let view_deltas: (i64, i64) = (nvx - ovx, nvy - ovy);
+
+        self.xt += view_deltas.0 / 2;
+        self.yt += view_deltas.1 / 2;
+    }
+    fn resize_window(&mut self, x: f32, y: f32) {
         self.window_size = (x, y);
+        self.resize_aux();
+    }
+    fn resize_zoom(&mut self) {
+        self.resize_aux();
+    }
+
+    fn center(&mut self) {
+        self.xt = self.grid_size.0 / 2;
+        self.yt = self.grid_size.1 / 2;
     }
 }
 
 impl Default for ViewParams {
     fn default() -> Self {
+        let cs = 10.0;
         let mut vp = ViewParams {
             window_size: DEFAULT_WINDOW_SIZE,
-            cell_size: (10.0, 10.0),
-            view_size: (0, 0),
-            grid_size: (100, 100),
+            cell_size: cs,
+            view_size: (
+                (DEFAULT_WINDOW_SIZE.0 / cs) as i64,
+                (DEFAULT_WINDOW_SIZE.1 / cs) as i64,
+            ),
+            grid_size: (200, 200),
             line_width: 1.0,
+            xt: 0,
+            yt: 0,
         };
 
-        vp.resize(vp.window_size.0, vp.window_size.1);
+        vp.resize_window(vp.window_size.0, vp.window_size.1);
 
         vp
     }
 }
-
-// const CELL_SIZE: (f32, f32) = (10.0, 10.0);
-// const GRID_SIZE: (usize, usize) = (100, 100);
-// const VIEW_SIZE: (usize, usize) = (200, 150);
-// const WINDOW_SIZE: (f32, f32) = (
-//     CELL_SIZE.0 * VIEW_SIZE.0 as f32,
-//     CELL_SIZE.1 * VIEW_SIZE.1 as f32,
-// );
-// const LINE_WIDTH: f32 = 1.0;
 
 const BG_COLOR: Color = Color::WHITE;
 const CELL_COLOR: Color = Color::BLACK;
@@ -79,8 +97,6 @@ struct State {
     actual_fps: f64,
     dirty: bool,
     cell_count: usize,
-    xt: i64,
-    yt: i64,
 }
 
 impl State {
@@ -90,15 +106,13 @@ impl State {
             view_params: ViewParams::default(),
             grid: SparseGrid::new(),
             show_grid: true,
-            fps: 1,
+            fps: 10,
             running: false,
             generation: 0,
             show_header: true,
             actual_fps: 0.0,
             dirty: true,
             cell_count: 0,
-            xt: 0,
-            yt: 0,
         }
     }
 
@@ -168,11 +182,11 @@ impl EventHandler<GameError> for State {
                     ctx,
                     &vec![
                         Point2 {
-                            x: i as f32 * view_params.cell_size.0,
+                            x: i as f32 * view_params.cell_size,
                             y: 0.0,
                         },
                         Point2 {
-                            x: i as f32 * view_params.cell_size.0,
+                            x: i as f32 * view_params.cell_size,
                             y: view_params.window_size.1,
                         },
                     ],
@@ -188,11 +202,11 @@ impl EventHandler<GameError> for State {
                     &vec![
                         Point2 {
                             x: 0.0,
-                            y: j as f32 * view_params.cell_size.1,
+                            y: j as f32 * view_params.cell_size,
                         },
                         Point2 {
                             x: view_params.window_size.0,
-                            y: j as f32 * view_params.cell_size.1,
+                            y: j as f32 * view_params.cell_size,
                         },
                     ],
                     view_params.line_width,
@@ -204,22 +218,18 @@ impl EventHandler<GameError> for State {
 
         for gc in self.grid.elements() {
             if let GridCoord::Valid(x, y) = gc {
-                let x = x + self.xt;
-                let y = y + self.yt;
+                let x = x + self.view_params.xt;
+                let y = y + self.view_params.yt;
                 if x >= 0
                     && x < view_params.view_size.0 as i64
                     && y >= 0
                     && y < view_params.view_size.1 as i64
                 {
+                    let cs = view_params.cell_size;
                     let rect = Mesh::new_rectangle(
                         ctx,
                         DrawMode::fill(),
-                        Rect::new(
-                            x as f32 * view_params.cell_size.0,
-                            y as f32 * view_params.cell_size.1,
-                            view_params.cell_size.0,
-                            view_params.cell_size.1,
-                        ),
+                        Rect::new(x as f32 * cs, y as f32 * cs, cs, cs),
                         CELL_COLOR,
                     )?;
                     canvas.draw(&rect, graphics::DrawParam::from(Point2 { x: 0.0, y: 0.0 }));
@@ -229,8 +239,13 @@ impl EventHandler<GameError> for State {
 
         if self.show_header {
             let mut text = Text::new(format!(
-                "FPS: {}, Pan: ({},{}), Generation: {}, Cells: {}",
-                self.fps, self.xt, self.yt, self.generation, self.cell_count
+                "FPS: {}, Pan: ({},{}), Cell size: {}, Generation: {}, Cells: {}",
+                self.fps,
+                self.view_params.xt,
+                self.view_params.yt,
+                self.view_params.cell_size,
+                self.generation,
+                self.cell_count
             ));
             text.set_scale(PxScale::from(50.0));
             canvas.draw(
@@ -274,20 +289,21 @@ impl EventHandler<GameError> for State {
                 self.fps -= 1;
             }
             if keycode == KeyCode::Up {
-                self.yt += pan_delta;
+                self.view_params.yt += pan_delta;
             }
             if keycode == KeyCode::Down {
-                self.yt += -1 * pan_delta;
+                self.view_params.yt += -1 * pan_delta;
             }
             if keycode == KeyCode::Left {
-                self.xt += pan_delta;
+                self.view_params.xt += pan_delta;
             }
             if keycode == KeyCode::Right {
-                self.xt += -1 * pan_delta;
+                self.view_params.xt += -1 * pan_delta;
             }
             if keycode == KeyCode::C {
-                self.xt = 0;
-                self.yt = 0;
+                self.view_params.xt = 0;
+                self.view_params.yt = 0;
+                // self.view_params.center();
             }
             if keycode == KeyCode::Delete || keycode == KeyCode::Back {
                 self.grid = SparseGrid::new();
@@ -298,6 +314,14 @@ impl EventHandler<GameError> for State {
             }
             if keycode == KeyCode::H {
                 self.show_header = !self.show_header;
+            }
+            if keycode == KeyCode::A && self.view_params.cell_size > 1.0 {
+                self.view_params.cell_size -= 1.0;
+                self.view_params.resize_zoom();
+            }
+            if keycode == KeyCode::S {
+                self.view_params.cell_size += 1.0;
+                self.view_params.resize_zoom();
             }
         }
 
@@ -312,7 +336,7 @@ impl EventHandler<GameError> for State {
     ) -> Result<(), GameError> {
         info!("Resize: {}, {}", width, height);
 
-        self.view_params.resize(width, height);
+        self.view_params.resize_window(width, height);
         Ok(())
     }
 }
